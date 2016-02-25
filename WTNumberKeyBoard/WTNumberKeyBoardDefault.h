322320f93b8fdd2a1c5f10f67dd3df177555448c
Sean-LWT <820378591@qq.com>
2016-02-25 12:11:44 +0800

    Added files via upload

---
+++ WTNumberKeyBoard/WTNumberKeyBoardDefault.h
@@ -0,0 +1,30 @@
+//
+//  WTNumberKeyBoardDefault.h
+//  WTNumberKeyBoard
+//
+//  Created by Sean on 16-2-12.
+//  Copyright (c) 2016年 Sean. All rights reserved.
+//
+
+#ifndef WTNumberKeyBoard_WTNumberKeyBoardDefault_h
+#define WTNumberKeyBoard_WTNumberKeyBoardDefault_h
+
+
+#define kWTNumberKeyBoardLineWidth 1
+
+#define kWTNumberKeyBoardNumFont [UIFont systemFontOfSize:27]
+
+#define kWTNumberKeyBoardColorNormal [UIColor whiteColor]
+
+#define kWTNumberKeyBoardColorHightlighted [UIColor colorWithRed:186.0/255 green:189.0/255 blue:194.0/255 alpha:1.0]
+
+#define kWTNumberKeyBoardColorLineColor [UIColor colorWithRed:188/255.0 green:192/255.0 blue:199/255.0 alpha:1]
+
+#define kWTNumberKeyBoardScreenWeight [UIScreen mainScreen].bounds.size.width
+#define kWTNumberKeyBoardScreenHeight [UIScreen mainScreen].bounds.size.height
+
+#define kWTNumberKeyBoardScale 54/480.0
+
+#define kWTNumberKeyBoardHeight 54
+
+#endif
